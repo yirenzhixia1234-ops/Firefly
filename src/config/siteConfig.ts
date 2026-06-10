7287978d3b43fbff12b2edf445226c3a8d087a2c
yirenzhixia1234-ops <yirenzhixia1234@gmail.com>
2026-06-10 23:59:23 +0800

    这是我的博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "菠萝包的博客",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "blog",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
@@ -90,7 +90,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-06-10",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
