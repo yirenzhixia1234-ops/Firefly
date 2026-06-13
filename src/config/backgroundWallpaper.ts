ccddcf36d0e614570b31536754a4c583e22b53d4
yirenzhixia1234-ops <yirenzhixia1234@gmail.com>
2026-06-14 01:08:51 +0800

    更换背景图片

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -39,25 +39,25 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	 */
 	src: {
 		// 桌面背景图片（支持单张或多张随机）
-		// desktop: "assets/images/DesktopWallpaper/d1.avif",
-		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
-		],
+		desktop: "assets/images/DesktopWallpaper/a1.jpg",
+		// desktop: [
+		// 	"assets/images/DesktopWallpaper/d1.avif",
+		// 	"assets/images/DesktopWallpaper/d2.avif",
+		// 	"assets/images/DesktopWallpaper/d3.avif",
+		// 	"assets/images/DesktopWallpaper/d4.avif",
+		// 	"assets/images/DesktopWallpaper/d5.avif",
+		// 	"assets/images/DesktopWallpaper/d6.avif",
+		// ],
 		// 移动背景图片（支持单张或多张随机）
-		// mobile: "assets/images/MobileWallpaper/m1.avif",
-		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
-		],
+		mobile: "assets/images/MobileWallpaper/a1.jpg",	
+		// mobile: [
+		// 	"assets/images/MobileWallpaper/m1.avif",
+		// 	"assets/images/MobileWallpaper/m2.avif",
+		// 	"assets/images/MobileWallpaper/m3.avif",
+		// 	"assets/images/MobileWallpaper/m4.avif",
+		// 	"assets/images/MobileWallpaper/m5.avif",
+		// 	"assets/images/MobileWallpaper/m6.avif",
+		// ],
 	},
 	// 横幅壁纸和全屏壁纸共享配置
 	common: {
@@ -70,17 +70,23 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "WonderfulEveryday",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"I dewll in possiblity-",
+				"A fairer House than Prose-",
+				"More numerous of Windows-",
+				"Superior -for Doors-",
+				"Of Chambers as the Cedars-",
+				"Impregnable of eye-",
+				"And for an everlasting Roof",
+				"The Gambrels of the Sky-",
+				"Of Visitors -the fairest-",
+				"For Occupation -This-",
+				"The spreading wide my narrow Hands",
+				"To gather Paradise-",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
